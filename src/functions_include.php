5efb2fceb376ca971c2cb61339c20cd2c8150ad5
Martin Poirier Théorêt <mpoiriert@gmail.com>
2023-05-07 11:32:03 -0400

    POR-6530 update support openswoole latest version

--- src/functions_include.php
+++ src/functions_include.php
@@ -6,3 +6,10 @@ declare(strict_types=1);
 if (!\function_exists('App\Bundle\SwooleBundle\Functions\replace_object_property')) {
     require __DIR__.'/functions.php';
 }
+
+if (!function_exists('swoole_cpu_num')) {
+    function swoole_cpu_num(): int
+    {
+        return \OpenSwoole\Util::getCPUNum();
+    }
+}
